3e271853df4c16d783d5ac56efc5223724bbe3df
Mohameddev73 <takhlitasimo@gmail.com>
2025-09-13 13:57:51 +0200

    locators

--- tests/locators.spec.ts
+++ tests/locators.spec.ts
@@ -14,4 +14,21 @@ test('Examples of playwright locators',async ({page})=>{
   //3.Get by Label
   await page.getByLabel('Username').fill('tomsmith');
   await page.getByLabel('password').fill('SuperSecretPassworld!');
+
+  //4.css selectors Examples
+  //1.Get by id css selector
+  await page.locator('#username').fill('selected_by_css'); 
+  //2.Get by tag attribute and attribute value
+  await page.locator('input[name="password"]').fill('selected_by_css');
+  //3.GEt by class name
+  await page.locator('.radius').click();
+  //Xpath Selector Examples
+  //1.Get by XPath ID
+  await page.locator('//input[@id="username"]').fill('selected_by_xpath');
+  //2. GET by Xpath with text match using contains
+  await page.locator('xpath=//button[contains(., "Login")]').click();
+  //3.store element by text for failed login and assert it is visible
+  const errorMessage = page.locator('//div[@id="flash"]');
+  await expect(errorMessage).toContainText('Your username is in valid!');
+  //
 })
